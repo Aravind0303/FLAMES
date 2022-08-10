aa6befaa2325f9a4ae990057ff1c8772440fc42a
Aravind0303 <46345979+Aravind0303@users.noreply.github.com>
2022-08-10 15:08:52 +0530

    Update Db.js

--- Db.js
+++ Db.js
@@ -1,27 +1,27 @@
-  var Config = {
-    apiKey: "AIzaSyAKSmikdo7DYIFxVsXli2qcGyxd7_To10g",
-    authDomain: "test-f68e0.firebaseapp.com",
-    databaseURL: "https://test-f68e0.firebaseio.com",
-    projectId: "test-f68e0",
-    storageBucket: "test-f68e0.appspot.com",
-    messagingSenderId: "745542438282",
-    appId: "1:745542438282:web:c5c87eb86bc206a1340c73",
-    measurementId: "G-ZP05FSMMBQ"
-  };
-  firebase.initializeApp(Config);
-  firebase.analytics();
+//   var Config = {
+//     apiKey: "AIzaSyAKSmikdo7DYIFxVsXli2qcGyxd7_To10g",
+//     authDomain: "test-f68e0.firebaseapp.com",
+//     databaseURL: "https://test-f68e0.firebaseio.com",
+//     projectId: "test-f68e0",
+//     storageBucket: "test-f68e0.appspot.com",
+//     messagingSenderId: "745542438282",
+//     appId: "1:745542438282:web:c5c87eb86bc206a1340c73",
+//     measurementId: "G-ZP05FSMMBQ"
+//   };
+//   firebase.initializeApp(Config);
+//   firebase.analytics();
 
-const  senddata = firebase.database().ref("Flames"); 
-document.getElementById("go").addEventListener('submit',sc)
+// const  senddata = firebase.database().ref("Flames"); 
+// document.getElementById("go").addEventListener('submit',sc)
 
-function sc(e)
-{
-e.preventDefault();
+// function sc(e)
+// {
+// e.preventDefault();
 
 // var fri=getVal("plz");
 // var fricrush=getVal("pleas");
 // send(fri,fricrush);
-}
+//}
 
 // function getVal(id)
 // {
